34d2bd316c59323b79bb7acb445676538e17feeb
Jason Gahr <jasongahr5@gmail.com>
2017-06-29 08:05:14 -0500

    My first commit

--- First Swift App/ViewController.swift
+++ First Swift App/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        if tapCount == 10 {
+            print("You have reached 10")
+        }
+    }
+    
 
+    
+    
+    @IBOutlet weak var theLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
+    // added to clean up place by Jason
+    
+    @IBOutlet weak var Changer1: UILabel!
+    
+    @IBAction func Change(_ sender: Any) {
+        Changer1.text = "Yes Abby is cool"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
